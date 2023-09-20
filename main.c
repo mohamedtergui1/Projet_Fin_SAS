02a664f025fc9b32782e25e155f12cebfd37ff88
mohamedtergui1 <144792669+mohamedtergui1@users.noreply.github.com>
2023-09-20 17:20:41 +0200

    ajout sort

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ Ajouter_une_tache(){
            FILE *file = fopen("taches.txt", "a");
            Taches tacheA;
            if (file == NULL) {
-    printf(file, "\nil ya une erreur !");
+    printf( "\nil ya une erreur !");
     fclose(file);
 }
           printf("\n=======================================================================================                                                   Ajouter une nouvelle tache\n=======================================================================================");
@@ -82,7 +82,7 @@ Ajouter_plusieurs_taches(){
        FILE *file = fopen("taches.txt", "a");
            Taches tacheA;
             if (file == NULL) {
-    printf(file, "\nil ya une erreur !");
+    printf( "\nil ya une erreur !");
     fclose(file);
 }
       fflush(file);
@@ -136,8 +136,16 @@ Afficher_les_tache(){
 
     fclose(file);
         int o;
-        printf("\n1-go to menu principal:");scanf("%d",&o);
-        if(o==1)menuPrincipal();else return 0;
+        printf("\n1-Trier les tâches par ordre alphabetique");
+        printf("\n2-Trier les tâches par deadline");
+        printf("\n3-Afficher les tâches dont le deadline est dans 3 jours ou moins");
+        printf("\naller au menu principal");
+        printf("\nentree votre choi :");scanf("%d",&o);
+        if(o==1)trier_alpha();
+        if(o==2)trier_deadline();
+        if(o==3)jour_deadline();
+        if(o==4)menuPrincipal();
+        else return ;
 }
 Modifier_une_tache(){
    FILE *file = fopen("taches.txt", "r");
@@ -214,6 +222,111 @@ Supprimer_identifiant(){
     }
 Rechercher_Taches(){}
 Statistiques(){}
+
+
+
+
+
+
+void trier_alpha(){
+    system("cls");
+     FILE *file = fopen("taches.txt", "r");
+           Taches *tacheA;
+           if (file == NULL) {
+    printf( "\nil ya une erreur !");
+    fclose(file);
+}
+  rewind(file);
+         tacheA=(Taches*)malloc(100*sizeof(Taches));
+         Taches tmptache;
+          int n =0;
+       while (fscanf(file, "%d\n%[^\n]\n%[^\n]\n%d\n%d\n%d\n%[^\n]\n", &tacheA[n].codeId, tacheA[n].titre, tacheA[n].description, &tacheA[n].date.jour, &tacheA[n].date.heurs, &tacheA[n].date.minute, tacheA[n].statut) == 7) {
+          n++;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+
+             if (strcmp(tacheA[i].titre,tacheA[j].titre)>0){
+                               // strcpy(tacheA[j].titre,tacheA[i].titre);
+                                //strcpy(tacheA[i].titre,tmpchar);
+                                tmptache=tacheA[i];
+                                tacheA[i]=tacheA[j];
+                                tacheA[j]=tmptache;
+                             }
+        }
+    }
+
+
+     printf("\n=======================================================================================\n                                                   Afficher les tache\n=======================================================================================\n");
+    printf("\n_______________________________________________________________________________________________________________________\n");
+    printf("|ID|           titre    |                   discription                                         |deadline |   status  |");
+    printf("\n|__|____________________|_______________________________________________________________________|_________|___________|\n");
+    for (int i=0;i<n;i++){
+        printf("|%2d|%-20s|%-71s|%02d-%02d-%02d |%-11s|\n", tacheA[i].codeId, tacheA[i].titre, tacheA[i].description, tacheA[i].date.jour, tacheA[i].date.heurs, tacheA[i].date.minute, tacheA[i].statut);
+        printf("|__|____________________|_______________________________________________________________________|_________|___________|\n");
+
+    }
+    free(tacheA);
+   fclose(file);
+      int o;
+        printf("\n1-go to menu principal:");scanf("%d",&o);
+        if(o==1)menuPrincipal();else return ;
+}
+
+
+trier_deadline(){    system("cls");
+     FILE *file = fopen("taches.txt", "r");
+           Taches *tacheA;
+           if (file == NULL) {
+    printf( "\nil ya une erreur !");
+    fclose(file);
+}
+  rewind(file);
+         tacheA=(Taches*)malloc(100*sizeof(Taches));
+         Taches tmptache;
+          int n =0;
+       while (fscanf(file, "%d\n%[^\n]\n%[^\n]\n%d\n%d\n%d\n%[^\n]\n", &tacheA[n].codeId, tacheA[n].titre, tacheA[n].description, &tacheA[n].date.jour, &tacheA[n].date.heurs, &tacheA[n].date.minute, tacheA[n].statut) == 7) {
+          n++;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+
+             if (tacheA[n].date.jour<tacheA[j].date.jour){
+                                tmptache=tacheA[i];
+                                tacheA[i]=tacheA[j];
+                                tacheA[j]=tmptache;
+                             }
+              else if (tacheA[n].date.heurs<tacheA[j].date.heurs&&tacheA[n].date.jour==tacheA[j].date.jour)   {
+                                tmptache=tacheA[i];
+                                tacheA[i]=tacheA[j];
+                                tacheA[j]=tmptache;
+              }
+              else if (tacheA[n].date.minute<tacheA[j].date.minute&&tacheA[n].date.heurs==tacheA[j].date.heurs&&tacheA[n].date.jour==tacheA[j].date.jour){
+                                tmptache=tacheA[i];
+                                tacheA[i]=tacheA[j];
+                                tacheA[j]=tmptache;
+              }
+        }
+    }
+
+
+     printf("\n=======================================================================================\n                                                   Afficher les tache\n=======================================================================================\n");
+    printf("\n_______________________________________________________________________________________________________________________\n");
+    printf("|ID|           titre    |                   discription                                         |deadline |   status  |");
+    printf("\n|__|____________________|_______________________________________________________________________|_________|___________|\n");
+    for (int i=0;i<n;i++){
+        printf("|%2d|%-20s|%-71s|%02d-%02d-%02d |%-11s|\n", tacheA[i].codeId, tacheA[i].titre, tacheA[i].description, tacheA[i].date.jour, tacheA[i].date.heurs, tacheA[i].date.minute, tacheA[i].statut);
+        printf("|__|____________________|_______________________________________________________________________|_________|___________|\n");
+
+    }
+    free(tacheA);
+   fclose(file);
+      int o;
+        printf("\n1-go to menu principal:");scanf("%d",&o);
+        if(o==1)menuPrincipal();else return ;}
+
+jour_deadline(){}
+
 int main()
 {
     menuPrincipal();
